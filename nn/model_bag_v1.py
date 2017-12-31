6d9fab2ff39d299215b5787542841fef8c5af218
kkipfer <kkipfer@student.ethz.ch>
2017-12-31 17:07:35 +0100

    some new ensemble model

---
+++ nn/model_bag_v1.py
@@ -0,0 +1,120 @@
+#Author: Kevin Kipfer
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+import os, sys, math, shutil, time, threading
+
+from utils import simple_arg_scope, batchnorm_arg_scope
+
+
+"""
+	ensemble of conv nets
+"""
+
+
+def classify(inputs, 
+             num_estimator,
+	     num_classes,
+             dropout_keep_prob=0.5,
+             middle_size=6,
+             gamma_decay = 1e-6,
+             weight_decay = 0.,
+	     scope=None,
+	     reuse=None,
+             is_training=True 
+	):
+        """
+	 model used to make predictions
+	 input: x -> shape=[None,bands,frames,num_channels]
+	 output: logits -> shape=[None,num_labels]
+        """
+        with slim.arg_scope(simple_arg_scope(weight_decay=weight_decay)): 
+        	#with slim.arg_scope(batchnorm_arg_scope()): 
+                      with slim.arg_scope([slim.batch_norm, slim.dropout],
+		                	is_training=is_training):
+                             with tf.variable_scope(scope, 'model_v1', [inputs], reuse=reuse) as scope:
+
+                                        net = tf.expand_dims(inputs, -1) #input needs to be in the format NHWC!! if there is only one channel, expand it by 1 dimension
+
+
+                                        net = slim.repeat(net, 2, slim.conv2d, 64, [3, 9], scope='conv1')
+                                        net = slim.max_pool2d(net, [2, 1], scope='pool1')
+                                        net = slim.repeat(net, 3, slim.conv2d, 128, [3, 5], scope='conv2')
+                                        net = slim.max_pool2d(net, [2, 1], scope='pool2')
+                                        net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
+                                        net = slim.max_pool2d(net, [2, 1], scope='pool3')
+                                        net = slim.flatten(net)
+                                        net = slim.fully_connected(net, 4096, scope='fc1')
+                                        net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+								 scope='dropout1')
+                                        net = slim.fully_connected(net, 4096, scope='fc2')
+                                        net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+								 scope='dropout2')
+                                        logits = slim.fully_connected(net, num_classes, scope='fc3', activation_fn=None)
+
+                                        return logits
+
+
+def loss_fkt(logits, y):
+        return tf.reduce_mean(tf.losses.softmax_cross_entropy(logits = logits, onehot_labels = y, label_smoothing=0.05)) 
+
+def gather(x, idx): #TODO use tf.gather instead
+        #x = tf.unstack(x)
+        #x =  [x[i] for i in list(idx)]
+        #return tf.stack(x)
+        return tf.gather(x, idx)
+
+
+def build_model(x, 
+		y,
+	        num_classes=2,
+		num_estimator=16,
+                subsample=0.5,
+                is_training=True,
+		reuse=None
+		):
+        """
+	 handle model. calculate the loss and the prediction for some input x and the corresponding labels y
+	 input: x shape=[None,bands,frames,num_channels], y shape=[None]
+	 output: loss shape=(1), prediction shape=[None]
+
+        """
+        #preprocess
+        y = slim.one_hot_encoding(y, num_classes)
+        
+        loss = 0
+        predictions=[]
+        batch_size = x.get_shape()[0].value
+        bx = x 
+        by = y
+
+        #models
+        for i in range(num_estimator):
+                if is_training:
+                   #sample from minibatch - instead of bootstrap / TODO something better?
+                   idx = np.random.randint(batch_size, size=(int(round(batch_size * subsample)),))
+                   bx = gather(x,idx)
+                   by = gather(y,idx)
+
+                logits = classify(bx, num_estimator=num_estimator, num_classes=num_classes, is_training=is_training, reuse=reuse, scope='H%d'%i)
+                loss += loss_fkt(logits, by)
+                predictions.append(tf.argmax(slim.softmax(logits),1))
+
+        with tf.name_scope('majority_vote' ):
+             predictions = tf.stack(predictions, 1)
+             predictions = tf.expand_dims(predictions, -1)
+             predictions = tf.expand_dims(predictions, -1)
+             predictions = slim.max_pool2d(predictions, [num_estimator, 1], num_estimator, scope='majority')
+             predictions = tf.squeeze(predictions)
+
+        return loss, predictions 	
+
+
+
+
+#Parameters
+TRAINABLE_SCOPES = ['conv1', 'fc2', 'fc3'] #bottom + top are trainable
+
+
+

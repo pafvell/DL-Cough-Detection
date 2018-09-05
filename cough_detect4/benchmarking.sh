9e2fb1e481baad1c121f4f18f5c086956b37b09d
mauriceweber <maurice.weber@hotmail.com>
2018-09-06 00:06:46 +0200

    save_graphs + script to run benchmark commands

---
+++ cough_detect4/benchmarking.sh
@@ -0,0 +1,55 @@
+export PATH="$PATH:$HOME/bin"
+
+TF_PATH=/Users/maurice/projects/machine_learning/tensorflow
+COUGH_PATH=/Users/maurice/projects/eth/deepLearning/DL-Cough-Detection/cough_detect4
+
+myExit() {
+  echo -en "\n*** Exiting ***\n\n"
+  pkill python
+  exit $?
+}
+ 
+trap myExit SIGINT
+
+
+# get shape and output layer name from user
+echo "model name: "
+read model_name
+echo "input_layer_shape: "
+read input_layer_shape
+echo "output_layer: "
+read output_layer
+
+
+# make sure frozen_graphs dir exists
+if [ ! -d "$COUGH_PATH/frozen_graphs" ]; then
+	mkdir $COUGH_PATH/frozen_graphs
+fi
+
+# make sure we're in tensorflow dir
+cd $TF_PATH
+
+
+echo -en "\n\n============================== $model_name ==============================\n"
+echo -en "\nfreezing model...\n"
+bazel-bin/tensorflow/python/tools/freeze_graph \
+	--input_graph="$COUGH_PATH/graphs/$model_name.pbtxt" \
+	--input_checkpoint="$COUGH_PATH/models/$model_name/cv1/checkpoint-100001" \
+	--output_graph="$COUGH_PATH/frozen_graphs/$model_name.pb" \
+	--output_node_names=$output_layer \
+	--input_binary=false
+
+echo -en "\nbenchmarking model...\n"
+bazel-bin/tensorflow/tools/benchmark/benchmark_model \
+	--graph="$COUGH_PATH/frozen_graphs/$model_name.pb"  \
+	--input_layer="Input" \
+	--input_layer_shape=$input_layer_shape \
+	--input_layer_type="float" \
+	--output_layer=$output_layer \
+	--show_run_order=false \
+	--show_time=false \
+	--show_memory=true \
+	--show_summary=true \
+	--show_flops=true
+echo -en "\n============================== end model $model_name ==============================\n\n"
+
---
+++ cough_detect4/save_graphs.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import tensorflow as tf
+import importlib
+import json
+import os
+
+ckpt_dir = "models"
+assert os.path.exists(ckpt_dir), "save checkpoints as ./models/<model_name>/cv1"
+
+
+for subdir in os.listdir(ckpt_dir):
+
+	model_ckpt_dir = os.path.join(ckpt_dir, subdir, 'cv1')
+
+	if os.path.isdir(model_ckpt_dir):
+
+		print(f"saving model {subdir} as graph...")
+		
+		with open(os.path.join(ckpt_dir, subdir, 'config.json')) as json_data_file:
+			config = json.load(json_data_file)
+			control_config = config["controller"]  # reads the config for the controller file
+			config_db = config["dataset"]
+			config_train = control_config["training_parameter"]
+
+		model_name = control_config["model"]
+		bands = config_db["BAND"]
+		size_cub = control_config["spec_size"]
+		num_estimator = config_train["num_estimator"]
+		num_filter = config_train["num_filter"]
+
+
+		latest_ckpt = tf.train.latest_checkpoint(model_ckpt_dir)
+		model = importlib.import_module(model_name)
+		sess_config = tf.ConfigProto(
+			log_device_placement=False,
+			allow_soft_placement=True
+		)
+		sess = tf.Session(config=sess_config)
+
+		input_tensor = tf.placeholder(tf.float32, shape=[bands, size_cub], name='Input')
+		x = tf.expand_dims(input_tensor, 0)
+		_, output_tensor = model.build_model(x, [1], num_estimator=num_estimator, num_filter=num_filter, is_training=False)
+
+		saver = tf.train.Saver()
+		saver.restore(sess, latest_ckpt)
+
+		#save the model
+		if not os.path.exists(os.path.join("tmp", subdir)):
+			os.makedirs("tmp/"+ subdir)
+
+		saver.save(sess, "tmp/" + subdir +"/" + subdir + ".ckpt")
+
+		#save the graph
+		tf.train.write_graph(sess.graph_def, 'graphs', subdir + '.pbtxt')

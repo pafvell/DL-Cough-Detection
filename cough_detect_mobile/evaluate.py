9bcf2bccacef041c93552933471fb423ab4f9ca6
mauriceweber <maurice.weber@hotmail.com>
2019-02-08 16:54:00 +0100

    func for ensemble eval

--- cough_detect_mobile/evaluate.py
+++ cough_detect_mobile/evaluate.py
@@ -4,15 +4,23 @@
 import argparse
 import importlib
 import json
+import tqdm
 
 import matplotlib.pyplot as plt
+import pandas as pd
 import sklearn.metrics as skmetrics
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, matthews_corrcoef
 
 from utils import *
 
 # ******************************************************************************************************************
-
+_CHECKPOINTS = {
+    'htc': './checkpoints/cnnv3l3_mobilehtc',
+    'iphone': './checkpoints/cnnv3l3_mobileiphone',
+    'samsung': './checkpoints/cnnv3l3_mobilesamsung',
+    'studio': './checkpoints/cnnv3l3_mobilestudio',
+    'tablet': './checkpoints/cnnv3l3_mobiletablet'
+}
 
 tf.set_random_seed(0)
 
@@ -66,7 +74,8 @@ def plot_roc(fpr, tpr, auc_roc_score):
     print("saved roc curve as {}".format(plot_filename))
 
 
-def classification_report(y_true, y_pred, y_probs=None, sanity_check=False, print_report=True, write_aucroccurve_tofile = False):
+def classification_report(y_true, y_pred, y_probs=None, sanity_check=False, print_report=True,
+                          write_aucroccurve_tofile=False):
     cm = confusion_matrix(y_true, y_pred)
     total = sum(sum(cm))
     acc = accuracy_score(y_true, y_pred)
@@ -91,8 +100,8 @@ def classification_report(y_true, y_pred, y_probs=None, sanity_check=False, prin
         if write_aucroccurve_tofile:
             df = pd.DataFrame({'fpr': fpr, 'tpr': tpr, 'thresholds': thresholds})
             df.to_csv("knn_roc_curve.csv")
-        #print('auc_roc_score: ', auc_roc_score)
-        #plot_roc(fpr, tpr, auc_roc_score)
+        # print('auc_roc_score: ', auc_roc_score)
+        # plot_roc(fpr, tpr, auc_roc_score)
 
     if sanity_check:
         print('(SANITY CHECK - our precision: %f vs sklearn precision: %f)' % (
@@ -173,7 +182,8 @@ def test(
     print()
     print('********************************************************************************')
     print('Evaluate over Everything:')
-    acc, sen, spe, prec, mcc, auc = classification_report(y, predictions, class_probs_list, write_aucroccurve_tofile = True)
+    acc, sen, spe, prec, mcc, auc = classification_report(y, predictions, class_probs_list,
+                                                          write_aucroccurve_tofile=True)
 
     X = list(zip(X, y, predictions))
     sources = ["studio", "iphone", "samsung", "htc", "tablet", "audio track"]
@@ -204,20 +214,20 @@ def test(
 
 
 def test_cv(device,
-        model_name=control_config["model"],
-        hop_length=config_db["HOP"],
-        bands=config_db["BAND"],
-        window=config_db["WINDOW"],
-        size_cub=control_config["spec_size"],
-        batch_size=config_train["batch_size"],
-        num_estimator=config_train["num_estimator"],
-        num_filter=config_train["num_filter"],
-        split_id=config_db["split_id"],
-        participants=config_db["test"],
-        sources=config_db["allowedSources"],
-        db_root_dir=config_db["DB_ROOT_DIR"],
-        checkpoint_dir=CKPT_DIR,
-        nfft=NFFT):
+            model_name=control_config["model"],
+            hop_length=config_db["HOP"],
+            bands=config_db["BAND"],
+            window=config_db["WINDOW"],
+            size_cub=control_config["spec_size"],
+            batch_size=config_train["batch_size"],
+            num_estimator=config_train["num_estimator"],
+            num_filter=config_train["num_filter"],
+            split_id=config_db["split_id"],
+            participants=config_db["test"],
+            sources=config_db["allowedSources"],
+            db_root_dir=config_db["DB_ROOT_DIR"],
+            checkpoint_dir=CKPT_DIR,
+            nfft=NFFT):
     checkpoint_dir = checkpoint_dir + device
     print('read checkpoints: %s' % checkpoint_dir)
     checkpoint_dir = checkpoint_dir + '/cv%d' % split_id
@@ -253,8 +263,8 @@ def test_cv(device,
                                       listOfParticipantsInTestset=config_db["test"],
                                       listOfParticipantsInValidationset=config_db["validation"],
                                       listOfAllowedSources=config_db["allowedSources"],
-                                      device_cv = True,
-                                      device = device
+                                      device_cv=True,
+                                      device=device
                                       )
 
     print('nr of samples coughing (test): %d' % len(X_cough))
@@ -276,8 +286,8 @@ def test_cv(device,
     print()
     print('********************************************************************************')
     print('Evaluate over Everything:')
-    acc, sen, spe, prec, mfcc, auc = classification_report(y, predictions, y_probs=class_probs_list, write_aucroccurve_tofile = False)
-
+    acc, sen, spe, prec, mfcc, auc = classification_report(y, predictions, y_probs=class_probs_list,
+                                                           write_aucroccurve_tofile=False)
 
     kinds = ["Close (cc)", "Distant (cd)", "01_Throat Clearing", "02_Laughing", "03_Speaking", "04_Spirometer"]
 
@@ -294,6 +304,125 @@ def test_cv(device,
             print('accuracy: ', acc)
 
     return acc, sen, spe, prec, mfcc, auc
+
+
+def test_ensemble(model_name=control_config["model"],
+                  hop_length=config_db["HOP"],
+                  bands=config_db["BAND"],
+                  window=config_db["WINDOW"],
+                  size_cub=control_config["spec_size"],
+                  batch_size=config_train["batch_size"],
+                  num_estimator=config_train["num_estimator"],
+                  num_filter=config_train["num_filter"],
+                  split_id=config_db["split_id"],
+                  participants=config_db["test"],
+                  sources=config_db["allowedSources"],
+                  db_root_dir=config_db["DB_ROOT_DIR"],
+                  checkpoint_dirs=_CHECKPOINTS,
+                  nfft=NFFT):
+
+    print("***************************** TESTING ENSEMBLE OF 5 CNNs *****************************")
+
+    checkpoints = {}
+    for mic, chckpt in checkpoint_dirs.items():
+        current_dir = checkpoint_dirs[mic] + '/cv%d' % split_id
+        checkpoints[mic] = tf.train.latest_checkpoint(current_dir)
+        if not checkpoints[mic]:
+            raise IOError(
+                'Invalid checkpoint path: %s! It is not possible to evaluate the model with checkpount' % current_dir)
+        print('restore checkpoint %s' % checkpoints[mic])
+
+    print('evaluate model:' + model_name)
+    model = importlib.import_module(model_name)
+
+    # get data
+    X_cough, X_other, _, _ = get_imgs(split_id=config_db["split_id"],
+                                      db_root_dir=config_db["DB_ROOT_DIR"],
+                                      listOfParticipantsInTestset=config_db["test"],
+                                      listOfParticipantsInValidationset=config_db["validation"],
+                                      listOfAllowedSources=config_db["allowedSources"])
+
+    print('nr of samples coughing (test): %d' % len(X_cough))
+    print('nr of samples NOT coughing (test): %d' % len(X_other))
+
+    X = X_cough + X_other
+    y = [1] * len(X_cough) + [0] * len(X_other)
+
+    # preprocess
+
+    # build graph
+    input_tensor = tf.placeholder(tf.float32, shape=[bands, size_cub], name='Input')
+    x = tf.expand_dims(input_tensor, 0)
+    _, output_tensor, logits = model.build_model(x, [1], num_estimator=num_estimator, num_filter=num_filter,
+                                                 is_training=False, include_logits=True)
+    class_probabilities = tf.squeeze(tf.nn.softmax(logits))
+    print(class_probabilities)
+
+    saver = tf.train.Saver()
+
+    # create list to store predictions of each model in the ensemble
+    predictions_per_model = list()
+
+    # Create the session that we'll use to execute the model
+    sess_config = tf.ConfigProto(
+        log_device_placement=False,
+        allow_soft_placement=True
+    )
+
+    for mic, chkpt in checkpoints.items():
+        print("evaluating {}...".format(mic))
+        with tf.Session(config=sess_config) as sess:
+            saver.restore(sess, chkpt)
+
+            class_probabilities_eval = []
+            for x in tqdm.tqdm(X):  # make_batches(X, batch_size):
+                x = preprocess(x, bands=bands, hop_length=hop_length, window=window, nfft=nfft)
+                # x = np.expand_dims(x, 0)
+                class_probs_sample = sess.run(class_probabilities, {input_tensor: x})
+                class_probabilities_eval.append(class_probs_sample)
+
+        predictions_per_model.append(class_probabilities_eval)
+
+    # store as numpy
+    predictions_per_model = np.array(predictions_per_model)
+
+    # average voting
+    probs_avg_vote = np.mean(predictions_per_model, axis=0)
+    probs_avg_vote_cough = list(probs_avg_vote[:, 1])
+    preds_avg_vote = np.argmax(probs_avg_vote, axis=1)
+
+    print()
+    print('********************************************************************************')
+    print('Evaluate over Everything:')
+    acc, sen, spe, prec, mcc, auc = classification_report(y, preds_avg_vote, probs_avg_vote_cough,
+                                                          write_aucroccurve_tofile=True)
+
+    X = list(zip(X, y, preds_avg_vote))
+    sources = ["studio", "iphone", "samsung", "htc", "tablet", "audio track"]
+    for mic in sources:
+        Xlittle = [x for x in X if mic in get_device(x[0])]
+        if len(Xlittle) > 0:
+            path, y_true, y_pred = zip(*Xlittle)
+            print()
+            print('********************************************************************************')
+            print('Evaluate ' + mic)
+            classification_report(y_true, y_pred)
+
+    kinds = ["Close (cc)", "Distant (cd)", "01_Throat Clearing", "02_Laughing", "03_Speaking", "04_Spirometer"]
+
+    for kind in kinds:
+        Xlittle = [x for x in X if kind in x[0]]
+        if len(Xlittle) > 0:
+            path, y_true, y_pred = zip(*Xlittle)
+            print()
+            print('********************************************************************************')
+            print('Evaluate ' + kind)
+            cm = confusion_matrix(y_true, y_pred)
+            acc = accuracy_score(y_true, y_pred)
+            print('Confusion Matrix: \n', cm)
+            print('accuracy: ', acc)
+
+    return acc, sen, spe, prec, mcc
 
 
 if __name__ == '__main__':
@@ -341,4 +470,5 @@ if __name__ == '__main__':
         print('#' * 100)
 
     else:
-        test()
+        # test()
+        test_ensemble()

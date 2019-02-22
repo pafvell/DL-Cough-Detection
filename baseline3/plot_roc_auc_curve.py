b91888065e9e90635216c9f90de7ab778bf55216
Filipe <fbarata@ethz.ch>
2019-02-22 19:15:36 +0100

    added latex to the fonts

--- baseline3/plot_roc_auc_curve.py
+++ baseline3/plot_roc_auc_curve.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 import sklearn.metrics
+from matplotlib import rc
 
-
-
+rc('text', usetex = True)
+rc('font',family='Times New Roman')
 
 
 
@@ -28,23 +29,28 @@ if __name__ == "__main__":
 
     plt.figure()
     lw = 2
-    plt.plot(df_cnn_single["fpr"], df_cnn_single["tpr"], color='b',
-             lw=lw, label='ROC curve CNN (area = %0.2f)' % auc_cnn_single)
-
-    plt.plot(df_cnn_ensemble["fpr"], df_cnn_ensemble["tpr"], color='c',
-             lw=lw, label='ROC curve CNN Ensemble (area = %0.2f)' % auc_cnn_ensemble)
+    plt.plot(df_knn["fpr"], df_knn["tpr"], color='g',
+             lw=lw, label=r"\textit{k}-NN  with Hu Moments (AUC = %0.2f)" % auc_knn)
 
 
     plt.plot(df_rf["fpr"],  df_rf["tpr"], color='darkorange',
-             lw=lw, label='ROC curve RF (area = %0.2f)' % auc_rf)
+             lw=lw, label=r"RF with PCA (AUC = %0.2f)" % auc_rf)
 
-    plt.plot(df_knn["fpr"],  df_knn["tpr"], color='g',
-             lw=lw, label='ROC curve kNN (area = %0.2f)' % auc_knn)
+    plt.plot(df_cnn_single["fpr"], df_cnn_single["tpr"], color='b',
+             lw=lw, label=r"CNN with Mel Spec (AUC = %0.2f)" % auc_cnn_single)
+
+    plt.plot(df_cnn_ensemble["fpr"], df_cnn_ensemble["tpr"], color='c',
+             lw=lw, label=r"Ensemble CNN  (AUC = %0.2f)" % auc_cnn_ensemble)
+
+
+
+
     plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic curve')
+    plt.title('Receiver Operating Characteristic Curves')
     plt.legend(loc="lower right")
+    plt.savefig("/home/dfilipe/Desktop/resultsAUC.eps", format='eps',dpi = 1000)
     plt.show()
